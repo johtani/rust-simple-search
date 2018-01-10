c307be87151a5e1d3e0d1d1ae533484a81c20d63
Jun Ohtani <johtani@gmail.com>
2018-01-10 20:20:16 +0900

    Implemented persist function, No tests

--- src/store/storage.rs
+++ src/store/storage.rs
@@ -1,38 +1,40 @@
+use std::io::prelude::*;
 use std::fs::File;
-use std::io::{BufReader, BufWriter};
+use std::io::BufWriter;
 use std::path::Path;
+use std::ptr;
 
 use index::InvertedIndex;
 use store::Encode;
 
 pub struct Storage {
     directory: String,
-    file: String,
+    file_name: String,
     encoder: Encode,
+    file_path: Path,
 }
 
 impl Storage {
     pub fn new (directory: &str, file: &str) -> Self {
         Storage {
-            directory: directory.to_string(),
-            file: file.to_string(),
+            directory: &directory.to_string(),
+            file_name: &file.to_string(),
             encoder: Encode::new(),
+            file_path: Path::new(&directory.to_string()).join(&file.to_string()),
         }
     }
 
     pub fn persist (&mut self, ii: InvertedIndex) {
-        let file_path = Path::new(&self.directory).join(self.file);
-        let file = match File::open(file_path){
+        let file = match File::open(&self.file_path){
             Ok(file) => file,
             Err(e) => {
-                println!("An error occurred while opening file {}:{}", file_path.to_string(), e);
+                println!("An error occurred while opening file {}:{}", &self.file_path.to_str().unwrap(), e);
                 return;
             }
         };
-
-        // to_JSON with Encode
-        // Open file
-        //
+        let mut file_writer = BufWriter::new(file);
+        file_writer.write(&self.encoder.encode(ii).as_bytes());
+        file_writer.flush();
     }
 
     pub fn open () {
